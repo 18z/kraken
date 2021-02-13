b858dbcc3cecc562c88bcaa598e5e36f90422835
KunYuChen <xspiritualx@gmail.com>
2021-02-13 13:16:13 +0800

    clear the unwanted

--- server/hosts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'hosts'
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'view/(?P<identifier>\w+)/$', views.profile, name='profile'),
-]
